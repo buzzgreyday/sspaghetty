ca321127d1fa48b2c9fe0daedcd90268dc907864
michael <micbr@protonmail.com>
2024-07-17 11:14:30 +0200

    Missing write config.sh during install

---
+++ config.sh
@@ -0,0 +1,23 @@
+# Directory paths
+TMP_DIR="$HOME/.tmp"
+IP_FILE="$TMP_DIR/last_ip.txt"
+DOWNLOAD_LOC="$HOME"
+INSTALL_LOC="/usr/local/bin"
+NODE_SCRIPT_HOME="$HOME/.sspaghetty"
+
+# Encrypted token file paths
+DO_TOKEN_FILE="$NODE_SCRIPT_HOME/do_auth_token.enc"
+HCLOUD_TOKEN_FILE="$NODE_SCRIPT_HOME/hcloud_auth_token.enc"
+
+# Color and formatting constants
+NO_FORMAT="\033[0m"
+F_BOLD="\033[1m"
+F_UNDERLINED="\033[4m"
+C_INDIANRED1="\033[38;5;203m"
+C_DEEPSKYBLUE3="\033[38;5;32m"
+C_YELLOW1="\033[38;5;226m"
+C_CHARTREUSE3="\033[38;5;76m"
+C_GREY_CENSOR1="\033[38;5;234m"
+C_GREY_CENSOR2="\033[48;5;233m"
+
+# Dynamically added values
